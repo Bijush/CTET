793fa8f4513c1166136783e9733080cbc2d92c31
bijush <38435589+Bijush@users.noreply.github.com>
2026-03-07 14:20:19 +0530

    Add files via upload

---
+++ js/GeneralMcq/Start/mock.js
@@ -0,0 +1,19 @@
+import { startMCQ } from "../../../engine/mcq_engine.js";
+import { mockQuestion } from "../../../data/mock_question.js";
+
+
+startMCQ({
+
+  data: mockQuestion,
+
+  subjectKey: "mock_subject",
+
+  limitKey: "mock_limit",
+
+  indexKey: "mock_q_index",
+
+  orderKey: "mock_q_order",
+
+  attemptKey: "mock_attempt_map"
+
+});
